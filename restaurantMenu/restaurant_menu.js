cc48fdea377ec0fd8a03452fd6426df6d998e542
vonzekiel <von.manaois@gmail.com>
2025-03-22 17:41:33 -0400

    iterating thru array in javascript

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,21 @@
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict- $22.99', 'Oatmeal- $21.99', 'Frittata- $15'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+    document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`
+})
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem
+    
+
+let desserItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    desserItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`
+}
+document.getElementById('dessertMenuItems').innerHTML = desserItem;
